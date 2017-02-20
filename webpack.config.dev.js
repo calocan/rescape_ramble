44efcd18ce2cebd9945e0fa8d645ae442f550eea
calocan <andy@likuski.org>
2017-02-20 14:18:38 -0800

    Fixed problem loading enzyme

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -58,9 +58,8 @@ module.exports = {
         })
     ],
     externals: {
-        'cheerio': 'window',
         'react/addons': 'react',
         'react/lib/ExecutionEnvironment': 'react',
-        'react/lib/ReactContext': 'react',
+        'react/lib/ReactContext': 'react'
     }
 };
